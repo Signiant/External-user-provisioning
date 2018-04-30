d69560116a67b7ab2d1dfdcadba3ea6d8eb2bb92
Kevin Haggerty <khaggerty@signiant.com>
2018-04-30 11:12:49 -0400

    Print error message if authenication fails with Slack

--- project/plugins/slack/__init__.py
+++ project/plugins/slack/__init__.py
@@ -15,21 +15,27 @@ def removeUser(email,configMap,allPermissions, plugin_tag):
     team = requests.get("https://slack.com/api/team.info?token=" + getApiToken(configMap,plugin_tag) )
     my_json = team.content.decode('utf8')
     data = json.loads(my_json)
-    teamId=data['team']['id']
+    if not 'error' in data and 'team' in data:
+        teamId=data['team']['id']
 
-    log = "Slack: "+email[:-13]+" was removed from Slack.\n"
-    instruction = email[:-13] + removalMessage(configMap,plugin_tag)
-    try:
-        #get user id
-        userId= requests.get(	"https://slack.com/api/auth.findUser?token=" + getApiToken(configMap,plugin_tag)+"&email="+email+"&team="+teamId )
-        my_json = userId.content.decode('utf8')
-        data = json.loads(my_json)
-        slackUserID = data['user_id']
+        log = "Slack: "+email[:-13]+" was removed from Slack.\n"
+        instruction = email[:-13] + removalMessage(configMap,plugin_tag)
+        try:
+            #get user id
+            userId= requests.get(	"https://slack.com/api/auth.findUser?token=" + getApiToken(configMap,plugin_tag)+"&email="+email+"&team="+teamId )
+            my_json = userId.content.decode('utf8')
+            data = json.loads(my_json)
+            slackUserID = data['user_id']
 
-        #disable user
-        user = requests.post("https://slack.com/api/users.admin.setInactive" + "?token=" + getApiToken(configMap,plugin_tag) + "&user="+slackUserID)
-    except Exception as error:
-        log = 'Slack: Remove from slack error: '+ email+' does not exist or is already inactive\n error: '+ str(error) +'\n'
-        instruction =  email+' was not found or is already inactive.'
+            #disable user
+            user = requests.post("https://slack.com/api/users.admin.setInactive" + "?token=" + getApiToken(configMap,plugin_tag) + "&user="+slackUserID)
+        except Exception as error:
+            log = 'Slack: Remove from slack error: '+ email+' does not exist or is already inactive\n error: '+ str(error) +'\n'
+            instruction =  email+' was not found or is already inactive.'
 
-    return getJsonResponse('Slack', email, log, instruction)
+        return getJsonResponse('Slack', email, log, instruction)
+    else:
+        error_data = None
+        if 'error' in data:
+            error_data = data['error']
+        print('ERROR communicating with slack: %s' % error_data)
