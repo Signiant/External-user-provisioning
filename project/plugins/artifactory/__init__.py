1170ca494d0b543cab2dac7f34042e17005dfbca
KseniaLL <36215894+KseniaLL@users.noreply.github.com>
2018-06-04 14:59:12 -0400

    Branch4 (#6)
    * Bitbucket is done
    
    * Bitbucket is done
    
    * final changes to bitbucket
    
    * slack and artifactory
    
    * slack and artifactory
    
    * slack final

--- project/plugins/artifactory/__init__.py
+++ project/plugins/artifactory/__init__.py
@@ -5,13 +5,15 @@ from project.plugin import inviteMessage, removalMessage
 def removeUser(email, configMap, allPermissions, plugin_tag):
     log = 'artifactory: '+ email+' removed alongside AD account \n'
     instruction= email.split('@', 1)[0] + removalMessage(configMap,plugin_tag)
-    return getJsonResponse('Artifactory', email, log, instruction)
+    done = True 
+    return getJsonResponse('Artifactory', email, log, instruction, done)
 
 
 def inviteUser(email, configMap, allPermissions, plugin_tag, name):
     log = 'Artifactory: Instruction sent in email.\n'
     instruction = inviteMessage(configMap,plugin_tag)
-    return getJsonResponse('Artifactory', email, log, instruction)
+    done = True
+    return getJsonResponse('Artifactory', email, log, instruction, done)
 
 
 
--- project/plugins/slack/__init__.py
+++ project/plugins/slack/__init__.py
@@ -8,34 +8,52 @@ def inviteUser(email,configMap,allPermissions, plugin_tag, name):
 
     log = 'Slack: Instruction sent in email.\n'
     instruction = inviteMessage(configMap,plugin_tag)
-    return getJsonResponse('Slack', email, log, instruction)
+    done = True
+    return getJsonResponse('Slack', email, log, instruction, done)
 
 def removeUser(email,configMap,allPermissions, plugin_tag):
+    instruction = ''
+    log = ''
+    done = False
+    userName = email.split('@', 1)[0]
     #get team id
     team = requests.get("https://slack.com/api/team.info?token=" + getApiToken(configMap,plugin_tag) )
     my_json = team.content.decode('utf8')
     data = json.loads(my_json)
-    if not 'error' in data and 'team' in data:
+
+    if 'error' in data:
+        if 'invalid_auth' in team.text:
+            log = plugin_tag + ' error: for user: ' + userName + '. Wrong api token'
+
+        if 'not_authed' in team.text:
+            log = plugin_tag + ' error: for user: ' + userName + '. No api token provided'
+
+        if 'fatal_error' in team.text:
+            log = plugin_tag + ' unexpected error: for user: ' + userName + '. could not be removed'
+
+    elif 'team' not in data:
+        log = plugin_tag + ' error: for user: ' + userName + '. Is not in the team'
+    else:
         teamId=data['team']['id']
 
-        log = "Slack: "+ email.split('@', 1)[0] +" was removed from Slack.\n"
-        instruction = email.split('@', 1)[0] + removalMessage(configMap,plugin_tag)
-        try:
-            #get user id
-            userId= requests.get(	"https://slack.com/api/auth.findUser?token=" + getApiToken(configMap,plugin_tag)+"&email="+email+"&team="+teamId )
+        #get user id
+        userId= requests.get("https://slack.com/api/auth.findUser?token=" + getApiToken(configMap,plugin_tag)+"&email="+email+"&team="+teamId )
+        if 'user_not_found' in userId.text:
+            log = plugin_tag + ' error: for user: ' + userName + ' was not found. Delete failed. '
+            print(log)
+        else:
             my_json = userId.content.decode('utf8')
             data = json.loads(my_json)
             slackUserID = data['user_id']
-
+            try:
             #disable user
-            user = requests.post("https://slack.com/api/users.admin.setInactive" + "?token=" + getApiToken(configMap,plugin_tag) + "&user="+slackUserID)
-        except Exception as error:
-            log = 'Slack: Remove from slack error: '+ email+' does not exist or is already inactive\n error: '+ str(error) +'\n'
-            instruction =  email+' was not found or is already inactive.'
+                user = requests.post("https://slack.com/api/users.admin.setInactive" + "?token=" + getApiToken(configMap,plugin_tag) + "&user="+slackUserID)
+                log = "Slack: " + userName + " was removed from Slack.\n"
+                instruction = userName + removalMessage(configMap, plugin_tag)
+                done = True
+            except Exception as error:
+                log = 'Slack: Remove from slack error: '+ userName+' could not be removed'
+                instruction =  email+' was not found or is already inactive.'
+                print(log)
 
-        return getJsonResponse('Slack', email, log, instruction)
-    else:
-        error_data = None
-        if 'error' in data:
-            error_data = data['error']
-        print('ERROR communicating with slack: %s' % error_data)
+    return getJsonResponse('Slack', email, log, instruction, done)
